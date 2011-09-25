de38da40d64733d106d76bd1ded57a815a5a492f
kirichenko <vakir@gmail.com>
2011-09-25 16:58:37 +0400

    finish codebreaker

--- codebreaker/lib/codebreaker/marker.rb
+++ codebreaker/lib/codebreaker/marker.rb
@@ -18,7 +18,19 @@ module Codebreaker
     end
 
     def number_count
-      (0..3).inject(0) { |count, index| count + (number_match?(index) ? 1 : 0) }
+      total_count - exact_count
+    end
+
+    def total_count
+      secret = @secret.split ''
+      @guess.split('').inject(0) do |count, n|
+        count + (delete_first(secret, n) ? 1 : 0)
+      end
+    end
+
+    def delete_first(code, n)
+      index = code.index(n)
+      code.delete_at index if index
     end
   end
 end
--- codebreaker/spec/marker_spec.rb
+++ codebreaker/spec/marker_spec.rb
@@ -53,6 +53,13 @@ module Codebreaker
           marker.number_count.should == 1
         end
       end
+
+      context "with 1 exact match duplicated in guess" do
+        it "returns 0" do
+          marker = Marker.new '1234', '1155'
+          marker.number_count.should == 0
+        end
+      end
     end
   end
 end
