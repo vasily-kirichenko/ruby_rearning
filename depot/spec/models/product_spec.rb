8e4f2c241a58ab7dac9f9c01a270123021bdea79
naceka <svp@Mac.local>
2011-09-27 21:14:23 +0400

    finish depot

--- depot/spec/models/product_spec.rb
+++ depot/spec/models/product_spec.rb
@@ -4,6 +4,6 @@ describe Product do
   it "product attributes must not be empty" do
     subject = Product.new
     subject.valid?.should be_false
-    subject.errors.should have :title
+    subject.errors.should include(:title)
   end
 end
